687323e495566ad55e1e07d498d5125227b7df0e
Sgh1101760594 <1101760594@qq.com>
2021-05-14 20:27:19 +0800

    filx:修复

---
+++ src/js/advertising.js
@@ -0,0 +1,4 @@
+// 引入公共设置
+require('../css/common.css')
+// 引入css文件
+require('../css/advertising.css');
--- webpack.config.js
+++ webpack.config.js
@@ -11,9 +11,10 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 module.exports = {
     // 入口 引出的js文件
     entry: {
-        home:'./src/js/home.js',
-        login:'./src/js/login.js',
-        pref:'./src/js/pref.js'
+        home: './src/js/home.js',
+        login: './src/js/login.js',
+        pref: './src/js/pref.js',
+        advertising: './src/js/advertising.js'
     },
 
     // 出口  生成的js文件
@@ -24,29 +25,49 @@ module.exports = {
     },
     //编译器 loader
     module: {
-        rules: [
-            { test: /\.css$/, use: [MiniCssExtractPlugin.loader, 'css-loader'] },
-            { test: /\.less$/, use: [MiniCssExtractPlugin.loader, 'css-loader', 'less-loader'] },
+        rules: [{
+                test: /\.css$/,
+                use: [{
+                    loader: MiniCssExtractPlugin.loader,
+                    options: {
+                        publicPath: '../'
+                    }
+                }, 'css-loader', 'postcss-loader']
+            },
+            {
+                test: /\.less$/,
+                use: [{
+                    loader: MiniCssExtractPlugin.loader,
+                    options: {
+                        publicPath: '../'
+                    }
+                }, 'css-loader', 'postcss-loader', 'less-loader']
+            },
             { test: /\.jpg|png|gif$/, loader: 'url-loader', options: { name: '[hash:8].[ext]', limit: 20 * 1024, esModule: false, outputPath: 'img' } },
             { test: /\.html$/, loader: 'html-loader' }
         ]
     },
     // 插件
     plugins: [
-        new HtmlWebpackPlugin({   //配置html打包的插件
-            template: './src/page/home.html',         //以哪个html文件作为打包的模板
-            filename:'home.html',
-            chunks:['home']                        //该html文件使用了哪些入口js文件
+        new HtmlWebpackPlugin({ //配置html打包的插件
+            template: './src/page/home.html', //以哪个html文件作为打包的模板
+            filename: 'home.html',
+            chunks: ['home'] //该html文件使用了哪些入口js文件
         }),
-        new HtmlWebpackPlugin({   //配置html打包的插件
-            template: './src/page/login.html',         //以哪个html文件作为打包的模板
-            filename:'login.html',
-            chunks:['login']
+        new HtmlWebpackPlugin({ //配置html打包的插件
+            template: './src/page/login.html', //以哪个html文件作为打包的模板
+            filename: 'login.html',
+            chunks: ['login']
         }),
-        new HtmlWebpackPlugin({   //配置html打包的插件
-            template: './src/page/pref.html',         //以哪个html文件作为打包的模板
-            filename:'pref.html',
-            chunks:['pref']
+        new HtmlWebpackPlugin({ //配置html打包的插件
+            template: './src/page/pref.html', //以哪个html文件作为打包的模板
+            filename: 'pref.html',
+            chunks: ['pref']
+        }),
+        new HtmlWebpackPlugin({ //配置html打包的插件
+            template: './src/page/advertising.html', //以哪个html文件作为打包的模板
+            filename: 'advertising.html',
+            chunks: ['advertising']
         }),
         new MiniCssExtractPlugin({
             filename: 'css/[name].css' // 输出到css文件夹里
@@ -61,10 +82,10 @@ module.exports = {
     devServer: {
         contentBase: path.resolve(__dirname, 'dist'), // 启动服务器目录
         compress: true, // 启动gzip
-        port: 8080,  // 端口  8080 80  8081 8082
+        port: 8080, // 端口  8080 80  8081 8082
         open: true, // 自动打开服务
         publicPath: '/', // 静态资源查找路径
-        openPage: 'home.html', // 打开的页面
+        openPage: 'advertising.html', // 打开的页面
     },
     target: 'web', // 目标是浏览器
 
